aa7f609c8bf01a198f2eaab8fe83150781f493ca
Mel Incognito <melxincognito@gmail.com>
2022-02-01 21:36:22 -0700

    margin fix

--- src/components/ui-layout/Layout.jsx
+++ src/components/ui-layout/Layout.jsx
@@ -4,7 +4,7 @@ import Footer from "./Footer";
 
 export default function Layout(props) {
   const mainDivStyle = {
-    margin: "70px 0px 10px",
+    margin: "70px 0px 15px",
     overflow: "scroll",
   };
 
